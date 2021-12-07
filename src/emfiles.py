634c20ef992614925a4d5e0ef11eb8f2ba32a04a
Bogdan Toader <bogdan.toader@yale.edu>
2021-12-07 16:37:53 -0500

    refactored the star file reading and fixed some issues wth the griddng

--- src/emfiles.py
+++ src/emfiles.py
@@ -1,11 +1,62 @@
+import time
 import numpy as np
 from external.pyem.pyem import star
 from src.ctf import get_ctf_params_from_df_row
 import mrcfile
 
-# TODO: need to check if I should use IMAGE_PATH and IMAGE_INDEX instead of
-# IMAGE_ORIGINAL_PATH and IMAGE_ORIGINAL_INDEX
-# And make sure this is correct anyway
+
+
+def load_data(data_dir, star_file):
+    """Load all the required information from star files and mrcs files.
+
+    Parameters:
+    ----------
+    data_dir : string
+               Directory containing the star file
+    star_file: string
+               Name of the star file (relative to the directory path).
+    Returns:
+
+    """
+    
+    df = star.parse_star(data_dir + star_file, keep_index = False)
+
+    print("load_data: number of partcles: ", len(df))
+    t0 = time.time()
+    imgs, pixel_size, angles, shifts, ctf_params = get_data_from_df(df, data_dir)
+    t1 = time.time()
+    print("load_data: data loaded, time: ", t1-t0) 
+
+    imgs_f = np.array([np.fft.fft2(np.fft.ifftshift(img)) for img in imgs])
+    t2 = time.time()
+    print("load_data: FFT of data, time: ", t2-t1)
+
+    params = {'ctf_params' : ctf_params,
+              'pixel_size' : pixel_size,
+              'angles'     : angles,
+              'shifts'     : shifts}
+
+    return imgs_f, params
+
+# TODO: write tests for this function and make with work with odd dimensions too.
+def crop_fourier_images(imgs, nx):
+    """Given an N x nx0 x nx0 array of N images of dimension nx0 x nx0 in the 
+    frequency space with the standard ordering, crop the high-frequency entries 
+    to reduce the image to the dimensions nx x nx. 
+    The cropped images are returned vectorized."""
+    
+    N = imgs.shape[0]
+    mid = imgs.shape[-1]/2
+
+    imgs_cropped = np.zeros([N, nx, nx], dtype = np.complex64)
+    for i, f in enumerate(imgs):
+        img = np.fft.fftshift(f)
+        imgs_cropped[i] = np.fft.ifftshift(
+                img[int(mid-nx/2):int(mid+nx/2), int(mid-nx/2):int(mid+nx/2)])
+
+    imgs_cropped = imgs_cropped.reshape(N, -1)
+
+    return imgs_cropped
 
 def get_data_from_df(df, data_dir):
     """Given a data frame as returned by star.parse_star, extract the useful
