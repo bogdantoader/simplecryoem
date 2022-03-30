62407a5e358d4e22ba2dfa41757f4406323f14f4
Bogdan Toader <bogdan.toader@yale.edu>
2022-03-30 17:42:40 -0400

    working version of vol residual nb

--- src/residual.py
+++ src/residual.py
@@ -11,8 +11,6 @@ def get_volume_residual(imgs, angles, sigma_noise, x_grid, slice_func_array, N_b
 
     #coords, resid =  voxel_wise_resid_fun(v, angles, shifts, ctf_params, imgs, sigma_noise, x_grid, slice_func_array)
 
-    #coords, resid =  voxel_wise_resid_from2d_fun(imgs, angles, sigma_noise, x_grid, slice_func_array)
-
     N_batch_small=10 
 
     angles_batches = np.array_split(angles, N_batch_small)
@@ -43,45 +41,23 @@ def get_volume_residual(imgs, angles, sigma_noise, x_grid, slice_func_array, N_b
 
     nx = x_grid[1].astype(jnp.int32)
 
-    @jax.jit
-    def get_v_resid(v_idx, resid):
-        v_resid_sum = jnp.zeros([nx,nx,nx])
-        v_resid_counts = jnp.zeros([nx,nx,nx])
-    
-        #for i in jnp.arange(v_idx.shape[0]):
-        #    v_resid_sum = v_resid_sum.at[tuple(v_idx[i])].add(resid[i])
-        #    v_resid_counts = v_resid_counts.at[tuple(v_idx[i])].add(1)
 
-        def body_fun(i, rsc):
-            v_resid_sum, v_resid_counts = rsc
-            v_resid_sum = v_resid_sum.at[tuple(v_idx[i])].add(resid[i])
-            v_resid_counts = v_resid_counts.at[tuple(v_idx[i])].add(1)
-            return (v_resid_sum, v_resid_counts) 
+    # Make sure the batches are on the CPU.
+    vol_idx_batches = np.array_split(np.array(nn_vol_idx), N_batches)
+    resid_batches = np.array_split(np.array(resid), N_batches)
 
-        v_resid_sum, v_resid_counts = jax.lax.fori_loop(0, v_idx.shape[0], body_fun, (v_resid_sum, v_resid_counts))
-        
-        return v_resid_sum, v_resid_counts
+    v_resid_sum = np.zeros([nx,nx,nx])
+    v_resid_counts = np.zeros([nx,nx,nx])
 
-    def get_v_resid_batch(vol_idx, resid, N_batches, get_v_resid):
-        # Make sure the batches are on the CPU.
-        vol_idx_batches = np.array_split(np.array(vol_idx), N_batches)
-        resid_batches = np.array_split(np.array(resid), N_batches)
+    for i in tqdm(range(N_batches)):
+        vrs, vrc = get_v_resid(vol_idx_batches[i], resid_batches[i], np.zeros([nx,nx,nx]))
 
-        v_resid_sum = np.zeros([nx,nx,nx])
-        v_resid_counts = np.zeros([nx,nx,nx])
+        v_resid_sum += vrs
+        v_resid_counts += vrc
 
-        for i in tqdm(range(N_batches)):
-            vrs, vrc = get_v_resid(vol_idx_batches[i], resid_batches[i])
+    # Add 1e-16 to avoid NaN when counts=0.
+    v_resid = v_resid_sum/(v_resid_counts+1e-16)
 
-            v_resid_sum += vrs
-            v_resid_counts += vrc
-
-        # Add 1e-16 to avoid NaN when counts=0.
-        v_resid = v_resid_sum/(v_resid_counts+1e-16)
-
-        return v_resid
-
-    v_resid = get_v_resid_batch(nn_vol_idx, resid, N_batches, get_v_resid)
     return v_resid
 
 
@@ -105,3 +81,24 @@ def voxel_wise_resid_from2d_fun(imgs, angles, sigma_noise, x_grid, slice_func_ar
 
     return proj_coords, resid
 
+
+
+@jax.jit
+def get_v_resid(v_idx, resid, zero_vol):
+    nx = zero_vol.shape[0]
+    v_resid_sum = jnp.zeros([nx,nx,nx])
+    v_resid_counts = jnp.zeros([nx,nx,nx])
+
+    #for i in jnp.arange(v_idx.shape[0]):
+    #    v_resid_sum = v_resid_sum.at[tuple(v_idx[i])].add(resid[i])
+    #    v_resid_counts = v_resid_counts.at[tuple(v_idx[i])].add(1)
+
+    def body_fun(i, rsc):
+        v_resid_sum, v_resid_counts = rsc
+        v_resid_sum = v_resid_sum.at[tuple(v_idx[i])].add(resid[i])
+        v_resid_counts = v_resid_counts.at[tuple(v_idx[i])].add(1)
+        return (v_resid_sum, v_resid_counts) 
+
+    v_resid_sum, v_resid_counts = jax.lax.fori_loop(0, v_idx.shape[0], body_fun, (v_resid_sum, v_resid_counts))
+    
+    return v_resid_sum, v_resid_counts
